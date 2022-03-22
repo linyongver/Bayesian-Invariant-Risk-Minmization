6e8f07494588be5c1ba0eb1ebbe61ba52a089e4f
Hanze Dong <hendrydong@gmail.com>
2022-03-22 16:38:12 +0800

    Update prepare_coco_dataset.sh

--- prepare_coco_dataset.sh
+++ prepare_coco_dataset.sh
@@ -1,10 +1,10 @@
 mkdir -p data_dir
 mkdir -p data_dir/SPCOCO
 mkdir -p data_dir/coco
-wget http://images.cocodataset.org/zips/train2017.zip -P data_dir/coco/train2017.zip
-wget http://images.cocodataset.org/zips/val2017.zip -P data_dir/coco/val2017.zip
-wget http://images.cocodataset.org/zips/test2017.zip -P data_dir/coco/test2017.zip
-wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip -P data_dir/coco/annotations_trainval2017.zip
+wget http://images.cocodataset.org/zips/train2017.zip -P data_dir/coco/
+wget http://images.cocodataset.org/zips/val2017.zip -P data_dir/coco/
+wget http://images.cocodataset.org/zips/test2017.zip -P data_dir/coco/
+wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip -P data_dir/coco/
 unzip data_dir/coco/train2017.zip -d data_dir/coco
 unzip data_dir/coco/val2017.zip -d data_dir/coco
 unzip data_dir/coco/test2017.zip -d data_dir/coco
